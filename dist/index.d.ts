eea468404d313499ab7b544fa2a2368d81560377
Liuhaoran Huang <haoran@advance.ai>
2019-02-14 15:51:23 +0800

    fix type

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,48 +1,4 @@
-export interface IDictionaryType {
-    [x: string]: any;
-}
-export declare type IMetaSPAProvider<P extends IDictionaryType> = {
-    [K in keyof P]?: P[K];
-} & {
-    MetaSPA: MetaSPACore<P>;
-};
-export interface IMetaRegistration<P extends IDictionaryType> {
-    namespace: string;
-    entries: string | string[];
-    providers: Array<{
-        symbol: keyof IMetaSPAProvider<P>;
-        module: () => any;
-    }>;
-    onLoad: (module: any, context: MetaSPACore<P>) => any;
-    unMount: (module: any, context: MetaSPACore<P>) => any;
-}
-export declare type IMetaSPALoadFunction = (config: {
-    namespace: string;
-    modules: any;
-}) => Promise<void>;
-export declare class MetaSPACore<P extends IDictionaryType> {
-    static getInstance: () => MetaSPACore<any>;
-    runTime: IDictionaryType;
-    providers: IMetaSPAProvider<P>;
-    registeredModules: IDictionaryType;
-    metaSPALoad: IMetaSPALoadFunction;
-    registrations: {
-        [x: string]: IMetaRegistration<P>;
-    };
-    register<T extends IDictionaryType>(config: IMetaRegistration<T>): this;
-    private _loadModuleAsync;
-    loadModule(namespace: string): this;
-    private _unMountModuleAsync;
-    unMountModule(namespace: string): this;
-}
-declare global {
-    interface Window {
-        metaSPA: MetaSPACore<any>;
-        metaSPALoad: IMetaSPALoadFunction;
-        metaSPAProvider: IMetaSPAProvider<any>;
-    }
-}
-declare const metaSPA: MetaSPACore<any>;
+declare const metaSPA: import("@metaspa/core").default<any>;
 declare const bootstrap: (config: {
     namespace: string;
     modules: any;
